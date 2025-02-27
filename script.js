59a33b8dbdadd336d047a04d532791f83dcf4517
Prashant Pal <palprashant465@gmail.com>
2025-02-27 16:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const makeChange = (c) => {
-  // your name here
+    let obj = {};
+    obj['q'] = Math.floor(c/25);
+    c=c%25;
+    obj['d'] = Math.floor(c/10);
+    c=c%10;
+    obj['n'] = Math.floor(c/5);
+    c=c%5;
+    obj['p'] = c;
+    console.log(obj);
+	
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
